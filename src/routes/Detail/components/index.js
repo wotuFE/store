d2c6b73dd83045866d157ab40addf92a8e70fbbc
huruqing <40466911@qq.com>
2017-04-24 06:53:21 +0800

    aaaa

---
+++ src/routes/Detail/components/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from '../../../components/Header'
+import './index.scss'
+
+class Register extends React.Component {
+	constructor(props) {
+		debugger;
+		super(props);
+	}
+	render() {
+		return(
+			<div className="wrap">
+        		详情(产品id为: {this.props.id})
+        	</div>
+		);
+	}
+}
+
+export default Register
